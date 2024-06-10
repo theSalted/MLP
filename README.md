3bbd2216cbd397b55b63a780b62ad565f99b57e5
Yuhao Chen <yuhao.chen2000@icloud.com>
2024-06-11 05:34:54 +0800

    Visualize results with Swift Charts

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Work In Progress
 
 A very simple Mult-Layer Perceptron with [MLX](https://github.com/ml-explore/mlx-swift).
 
+Data visualization via Swift Charts and SwiftUI.
+
+see reference results in Reference folder.
+
 - Note: I use this project to get faimilar with MLX, so it doesn't utilize existing implementation of some methods in MLXNN and MLXOptimizer.
 
 ## Build Run in CLI
--- Sources/MLP/MLP.swift
+++ Sources/MLP/MLP.swift
@@ -6,7 +6,8 @@
 import MLX
 import MLXRandom
 import Cmlx
-
+import Charts
+import SwiftUI
 
 public func tensordot(
     _ a: MLXArray, _ b: MLXArray, axes: Int, stream: StreamOrDevice = .default
@@ -34,7 +35,7 @@ struct MLP {
     func forwardPass(
         X: MLXArray,
         weightsInputHidden: MLXArray,
-        weightsHiddenOutput: MLXArray) 
+        weightsHiddenOutput: MLXArray)
     -> (output: MLXArray,
         hiddenLayerActivations: MLXArray) {
         
@@ -52,7 +53,7 @@ struct MLP {
         output: MLXArray,
         weightsHiddenOutput: MLXArray,
         weightsInputHidden: MLXArray,
-        hiddenLayerActivation: MLXArray) 
+        hiddenLayerActivation: MLXArray)
     -> (hiddenToOutputWeightGradients: MLXArray,
         inputToHiddenWeightGradients: MLXArray)
     {
@@ -69,7 +70,7 @@ struct MLP {
                 inputToHiddenWeightGradients: inputToHiddenWeightGradients)
     }
     
-    func train(X: MLXArray, y: MLXArray) {
+    @MainActor func train(X: MLXArray, y: MLXArray) {
         let inputLayerNeurons = 1
         let hiddenLayerNeurons = 3
         let outputLayerNeurons = 1
@@ -80,7 +81,7 @@ struct MLP {
         let lr = 0.1
         let epochs = 1000
         
-        var losses: [MLXArray] = []
+        var losses: [Float32] = []
         
         for ep in 0..<epochs {
             let (output_, hiddenLayerActivation) = forwardPass(X: X, weightsInputHidden: weightsInputHidden, weightsHiddenOutput: weightsHiddenOutput)
@@ -101,14 +102,65 @@ struct MLP {
             if ep % 100 == 0 {
                 print("Error at epoch \(ep) is \(epochsLoss)")
             }
-            
-            losses.append(epochsLoss)
+            let loss: [Float32] = epochsLoss.asArray(Float32.self)
+            losses.append(loss[0] as Float)
         }
         
         let (finalPred, _) = forwardPass(X: X, weightsInputHidden: weightsInputHidden, weightsHiddenOutput: weightsHiddenOutput)
         
-        print(finalPred)
+        print("Weights of the network are: ")
+        print("w13, w12, w14", weightsHiddenOutput.T)
+        print("w35, w25, w45", weightsInputHidden)
+        
+        
+        let lossChart = Chart(losses.indices, id: \.self) { index in
+            LineMark(x: .value("Epochs", index), y: .value("Losses", losses[index]))
+        }
+        plot(lossChart, name: "LossChart")
+        
+        let finalPredFloat: [Float] = finalPred.asArray(Float.self)
+        let finalPredChart = Chart(finalPredFloat.indices, id: \.self) { index in
+            LineMark(x: .value("Index", index), y: .value("Value", finalPredFloat[index])).foregroundStyle(.red)
+        }
+        plot(finalPredChart, name: "Final Prediction Chart")
+        
+        let yTrainFloat: [Float] = y.asArray(Float.self)
+        print(yTrainFloat)
+        let yTrainChart = Chart(yTrainFloat.indices, id: \.self) { index in
+            LineMark(x: .value("Train Index", index), y: .value("Train Value", yTrainFloat[index])).foregroundStyle(.blue)
+        }
+        plot(yTrainChart, name: "YTrain Chart")
     }
+    
+    @MainActor
+    func plot(_ chart: Chart<some ChartContent>, name: String = "chartImage") {
+        let renderer = ImageRenderer(content:
+            chart
+            .chartYAxis {
+                AxisMarks(position: .automatic)
+            }
+            .chartXAxis {
+                AxisMarks(position: .automatic)
+            }
+            .padding(40)
+            .frame(width: 500, height: 500)
+            .background(Color.white)
+        )
+        if let chartImage = renderer.nsImage {
+            let fileURL = URL(filePath: "./\(name).png") // Replace with your desired path
+            if let tiffData = chartImage.tiffRepresentation,
+               let bitmap = NSBitmapImageRep(data: tiffData),
+               let pngData = bitmap.representation(using: .png, properties: [:]) {
+                do {
+                    try pngData.write(to: fileURL)
+                    print("Chart image saved successfully at \(fileURL.path)")
+                } catch {
+                    print("Failed to save chart image: \(error)")
+                }
+            }
+        }
+    }
+    
     
     private init() {}
     
@@ -116,10 +168,7 @@ struct MLP {
         let mlp = MLP.shared
         
         let XTrain = MLXArray(converting: [-3, -2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2.0, 3.0], [11, 1])
-        print(XTrain)
-        
         let yTrain = MLXArray(converting: [0.7312, 0.7339, 0.7438, 0.7832, 0.8903, 0.9820, 0.8114, 0.5937, 0.5219, 0.5049, 0.5002], [11, 1])
-        print(yTrain)
         
         mlp.train(X: XTrain, y: yTrain)
     }
