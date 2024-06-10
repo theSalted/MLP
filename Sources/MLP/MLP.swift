d1fd2296781862e82ce2d9e59b65dcab753f0253
Yuhao Chen <yuhao.chen2000@icloud.com>
2024-06-11 00:19:25 +0800

    Complete implementation

--- Sources/MLP/MLP.swift
+++ Sources/MLP/MLP.swift
@@ -5,6 +5,18 @@
 // Yes I know there is a MLXNN framework, but I want to get familiar with MLX first
 import MLX
 import MLXRandom
+import Cmlx
+
+
+public func tensordot(
+    _ a: MLXArray, _ b: MLXArray, axes: Int, stream: StreamOrDevice = .default
+) -> MLXArray {
+    MLXArray(
+        mlx_tensordot(
+            a.ctx, b.ctx, Int32(axes),
+            stream.ctx))
+}
+
 
 /// A dead simple multi-layer perceptron
 @main
@@ -19,9 +31,19 @@ struct MLP {
         return 1 / (1 + exp(-x))
     }
     
-    
-    func forwardPass(X: MLXArray, weightsInputHidden: MLXArray, weightsHiddenOutput: MLXArray) -> (MLXArray, MLXArray) {
-        return (MLXArray(), MLXArray())
+    func forwardPass(
+        X: MLXArray,
+        weightsInputHidden: MLXArray,
+        weightsHiddenOutput: MLXArray) 
+    -> (output: MLXArray,
+        hiddenLayerActivations: MLXArray) {
+        
+        let hiddenLayerActivations = sigmoid(tensordot(X, weightsInputHidden, axes: 1))
+        
+        let output = sigmoid(tensordot(hiddenLayerActivations, weightsHiddenOutput, axes: 1))
+        
+        return (output: output,
+                hiddenLayerActivations: hiddenLayerActivations)
     }
     
     func backwardPass(
@@ -30,9 +52,21 @@ struct MLP {
         output: MLXArray,
         weightsHiddenOutput: MLXArray,
         weightsInputHidden: MLXArray,
-        hiddenLayerActivation: MLXArray) -> (MLXArray, MLXArray)
+        hiddenLayerActivation: MLXArray) 
+    -> (hiddenToOutputWeightGradients: MLXArray,
+        inputToHiddenWeightGradients: MLXArray)
     {
-        return (MLXArray(), MLXArray())
+        let error = output - y
+        let errorToOutputWeightGradients = error * backdropSigmoid(output)
+        
+        let hiddenToOutputWeightGradients = tensordot(hiddenLayerActivation.T, errorToOutputWeightGradients, axes: 1)
+        
+        
+        let hiddenError = tensordot(errorToOutputWeightGradients, weightsHiddenOutput.T, axes: 1) * backdropSigmoid(hiddenLayerActivation)
+        let inputToHiddenWeightGradients = tensordot(X.T, hiddenError, axes: 1)
+        
+        return (hiddenToOutputWeightGradients: hiddenToOutputWeightGradients,
+                inputToHiddenWeightGradients: inputToHiddenWeightGradients)
     }
     
     func train(X: MLXArray, y: MLXArray) {
@@ -52,7 +86,7 @@ struct MLP {
             let (output_, hiddenLayerActivation) = forwardPass(X: X, weightsInputHidden: weightsInputHidden, weightsHiddenOutput: weightsHiddenOutput)
             let error = square(output_ - y) / 2
             
-            let (_, errorWrtWeightsInputHidden) = backwardPass(
+            let (hiddenToOutputWeightGradients, inputToHiddenWeightGradients) = backwardPass(
                 X: X,
                 y: y,
                 output: output_,
@@ -60,8 +94,8 @@ struct MLP {
                 weightsInputHidden: weightsInputHidden,
                 hiddenLayerActivation: hiddenLayerActivation)
             
-            weightsHiddenOutput = weightsHiddenOutput - lr * errorWrtWeightsInputHidden
-            weightsInputHidden = weightsInputHidden - lr * errorWrtWeightsInputHidden
+            weightsHiddenOutput = weightsHiddenOutput - lr * hiddenToOutputWeightGradients
+            weightsInputHidden = weightsInputHidden - lr * inputToHiddenWeightGradients
             
             let epochsLoss = mean(error)
             if ep % 100 == 0 {
@@ -86,7 +120,7 @@ struct MLP {
         
         let yTrain = MLXArray(converting: [0.7312, 0.7339, 0.7438, 0.7832, 0.8903, 0.9820, 0.8114, 0.5937, 0.5219, 0.5049, 0.5002], [11, 1])
         print(yTrain)
-            
+        
         mlp.train(X: XTrain, y: yTrain)
     }
 }
